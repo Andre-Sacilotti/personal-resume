43d77e2e310577c42995784c69200bff8a1b6120
André Sacilotti <andre.sacilotti@estudante.ufscar.br>
2021-02-18 13:32:00 -0300

    feat: Estrutura de pastas para navbar

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from '../../Components/Navbar/Navbar';
+
+const Home = () => (
+    <div>
+      <Navbar />
+      Teste
+    </div>
+);
+
+export default Home;

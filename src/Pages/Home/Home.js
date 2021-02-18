948b9411209a0c691670ecdedd9b7b8adb5ac8a8
André Sacilotti <andre.sacilotti@estudante.ufscar.br>
2021-02-18 13:47:38 -0300

    feat: Adição de fontes montserrat e definição de cores padrão no Home

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
 import Navbar from '../../Components/Navbar/Navbar';
 
+const HomeDiv = styled.div`
+  background-color: #191919;
+  min-height: 100vh;
+  width: 100vw;
+  color: #FFFFFF;
+  font-family: 'Montserrat', sans-serif;
+`;
+
 const Home = () => (
-    <div>
+    <HomeDiv>
       <Navbar />
       Teste
-    </div>
+    </HomeDiv>
 );
 
 export default Home;
